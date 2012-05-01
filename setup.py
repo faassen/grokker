5b6d4b56399a2926a82eacf95deb78052b3871e9
faassen <faassen@startifact.com>
2012-05-01 10:55:40 +0200

    Initial empty project.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='grokker',
+      version = '0.1dev',
+      description="A reformulation of Martian.",
+      author="Martijn Faassen",
+      author_email="faassen@startifact.com",
+      license="BSD",
+      packages=find_packages('grokker'),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools'],
+      entry_points="""
+      # Add entry points here
+      """,
+      )
